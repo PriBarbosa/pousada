47090ba11de4a3d7c446513156a120f888f09fab
Priscila Barbosa <priscilabarbosa1510@gmail.com>
2024-12-05 15:23:03 -0300

    atualizaçãodo aside/mudação de local na pagina

---
+++ contatos.js
@@ -0,0 +1,26 @@
+
+function calcularDiaria() {
+    var checkin = document.getElementById('checkin')
+    console.log(checkin.value)
+    var checkout = document.getElementById('checkout')
+    console.log(checkout.value)
+
+    // descobrir os dias
+    var dias = 0
+
+
+
+    // multiplicar valores por diarias
+    var diaria = 100
+    var resultado = dias * diaria
+    console.log(resultado)
+
+
+    // exibir resultado na tela
+    document.appendChild
+
+
+
+}
+
+
